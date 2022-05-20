b0fe2d9a9d406c0273df09d98f34182c42729c7b
Sebastian Bugge <vkbugge@hotmail.com>
2022-05-20 11:45:22 +0200

    Added styling to editable lables

--- src/main/java/org/ntnu/vsbugge/wargames/gui/decorators/EditableDecorator.java
+++ src/main/java/org/ntnu/vsbugge/wargames/gui/decorators/EditableDecorator.java
@@ -9,19 +9,34 @@ import org.ntnu.vsbugge.wargames.utils.funcinterfaces.StringSetter;
 import org.ntnu.vsbugge.wargames.utils.funcinterfaces.UnitSetter;
 
 public class EditableDecorator {
+    private static final String[] EDITABLE_STYLE_CLASSES = {"editable-label", "padded-light"};
+
     public static void makeEditable(Label label, IntSetter setter) {
         label.setOnMouseClicked(new IntInputDoubleClickSwapper(label, setter));
+        addStyleClass(label);
     }
 
     public static void makeEditable(Label label, StringSetter setter) {
         label.setOnMouseClicked(new StringInputDoubleClickSwapper(label, setter));
+        addStyleClass(label);
     }
 
     public static void makeEditable(Label label, UnitSetter setter) {
         label.setOnMouseClicked(new UnitSelectorDoubleClickSwapper(label, setter));
+        addStyleClass(label);
     }
 
     public static void makeNonEditable(Label label) {
         label.setOnMouseClicked(null);
+        removeStyleClass(label);
+    }
+
+    private static void removeStyleClass(Label label) {
+        label.getStyleClass().removeAll(EDITABLE_STYLE_CLASSES);
+    }
+
+    private static void addStyleClass(Label label) {
+        removeStyleClass(label);
+        label.getStyleClass().addAll(EDITABLE_STYLE_CLASSES);
     }
 }
